68e0ee1537c4b7a66480369de85bda6345569200
Ayodeji <ayoeluyemi2002@gmail.com>
2023-08-12 22:20:35 +0100

    Third commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,18 @@ function App() {
       <div class="container">
         <div class="share">
           <a href="https://ayodejieluyemi.great-site.net/">
-            <img src="./images/share-06.png" alt="" srcset="" />
+            <img src="../images/share-06.png" alt="" srcset="" />
           </a>
         </div>
 
         <div class="share-mobile">
           <a href="https://ayodejieluyemi.great-site.net/">
-            <img src="./images/dots-horizontal.png" alt="" srcset="" />
+            <img src="../images/dots-horizontal.png" alt="" srcset="" />
           </a>
         </div>
 
         <div class="profile">
-          <img src="./images/profile__img.png" alt="" srcset="" />
+          <img src="images/profile__img.png" alt="" srcset="" />
           <h1>John Doe</h1>
         </div>
         <div class="links">
